ecc5bfe3f95e0bd4680daad5ae28be7e20f88ca8
Noah Li <thenoahli06@gmail.com>
2025-11-03 13:18:55 -0600

    Added socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Socials: React.FC = () => {
-  return <div className="output-component"></div>;
+  return (
+    <div className="output-component">
+      <p>you can find me on:</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/noahli06/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/noahs_li/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Socials;
